657888953a261dc4a5dfc5bf2d4742de634c614d
Zhe Xian Lee <zxlee618@gmail.com>
2016-03-14 22:08:44 +1100

    Implements the UI and related functions

--- app/src/main/java/com/zhexian/magic8ball/MainActivity.java
+++ app/src/main/java/com/zhexian/magic8ball/MainActivity.java
@@ -1,47 +1,124 @@
 package com.zhexian.magic8ball;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, OnEditorActionListener {
 
-    private String mName;
-    private double mAge;
     private MagicEightBallModel mMagicEightBall;
+    private EditText mTxtQuestion;
+    private TextView mTxtResponse;
+    private ImageView mImgMagicEightBall;
+    private Button mBtnShake;
+    private RelativeLayout mRelativeLayoutMagicEightBall;
+
+    private AlphaAnimation mFadeOutAnimation;
+    private AlphaAnimation mFadeInAnimation;
+
+    private int[] magicEightBallImages = {
+            R.drawable.circle1,
+            R.drawable.circle2,
+            R.drawable.circle3,
+            R.drawable.circle4,
+            R.drawable.circle5,
+            R.drawable.circle6
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAge = 21;
-        mName = "Zhe Xian Lee";
+        mMagicEightBall = new MagicEightBallModel();
 
-        Log.i("INFO", "Zhe Xian Lee");
-        Log.i("INFO", String.format("%.2f", mAge));
-        Log.i("INFO", "My name is " + mName);
+        // Getting views
+        mTxtQuestion = (EditText) findViewById(R.id.txtQuestion);
+        mTxtResponse = (TextView) findViewById(R.id.txtResponse);
+        mImgMagicEightBall = (ImageView) findViewById(R.id.imgMagicEightBall);
+        mBtnShake = (Button) findViewById(R.id.btnShake);
+        mRelativeLayoutMagicEightBall = (RelativeLayout) findViewById(R.id.relativeLayoutMagicEightBall);
 
-        List<String> questions = Arrays.asList(
-                "Will I get full marks for this lab?",
-                "Will the Cronulla sharks receive a premiership this year?",
-                "Will I end up becoming a cat person when I get old?");
+        mBtnShake.setOnClickListener(this);
+        mTxtQuestion.setOnEditorActionListener(this);
 
-        List<String> myResponse = Arrays.asList(
-                "Whatever",
-                "😛");
+        mFadeOutAnimation = new AlphaAnimation(1, 0);
+        mFadeOutAnimation.setDuration(500);
 
-        mMagicEightBall = new MagicEightBallModel(myResponse);
+        mFadeInAnimation = new AlphaAnimation(0, 1);
+        mFadeInAnimation.setDuration(500);
 
-        for (int i = 0; i < questions.size(); i++) {
-            Log.i("INFO", questions.get(i));
-            Log.i("INFO", mMagicEightBall.tellFortune());
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mBtnShake) {
+            shakeMagicEightBall();
+        }
+    }
+
+    @Override
+    public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+        if (textView == mTxtQuestion && actionId == EditorInfo.IME_ACTION_GO) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(textView.getWindowToken(), 0);
+            shakeMagicEightBall();
+            return true;
+        }
+        return false;
+    }
+
+    public void shakeMagicEightBall() {
+        if (mTxtQuestion.getText().toString().trim().length() == 0) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle("No question asked");
+            alertDialogBuilder.setMessage("Please ask a question before shaking the magic eight ball.");
+            alertDialogBuilder.setPositiveButton("OK", null);
+            alertDialogBuilder.show();
+            return;
         }
 
-        Log.i("INFO", mMagicEightBall.toString());
+        Animation.AnimationListener animationListener = new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (animation == mFadeOutAnimation) {
+                    Random random = new Random(System.currentTimeMillis());
+                    mImgMagicEightBall.setImageResource(magicEightBallImages[random.nextInt(magicEightBallImages.length)]);
+                    mTxtResponse.setText(mMagicEightBall.tellFortune());
+
+                    mRelativeLayoutMagicEightBall.startAnimation(mFadeInAnimation);
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        };
+
+        mFadeOutAnimation.setAnimationListener(animationListener);
+        mFadeInAnimation.setAnimationListener(animationListener);
+
+        mRelativeLayoutMagicEightBall.startAnimation(mFadeOutAnimation);
     }
 }
